4a5ae896270ae12cc3733666aadaaba42d911a95
Alex Kennedy <rakennedyj18@gmail.com>
2020-12-16 10:46:03 -0500

    pushing initial changes

---
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -0,0 +1,36 @@
+package com.techelevator.tenmo.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techelevator.tenmo.dao.TransferDAO;
+import com.techelevator.tenmo.dao.UserDAO;
+import com.techelevator.tenmo.model.Balance;
+
+@RestController
+@PreAuthorize("isAuthenticated()")
+public class TransferController {
+	
+	@Autowired
+	TransferDAO transferDao;
+	
+	@Autowired
+	UserDAO userDao;
+	
+	@RequestMapping(path="/get-balance", method=RequestMethod.GET)
+	public Balance getBalance(Principal principal) {
+		
+		System.out.println("The following person is requesting a balance: " + principal.getName());
+		
+		int id = userDao.findIdByUsername(principal.getName());
+		Balance balance = transferDao.getBalance(id);
+		
+		return balance;
+	}
+
+}
---
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferSqlDAO.java
@@ -0,0 +1,39 @@
+package com.techelevator.tenmo.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import com.techelevator.tenmo.model.Balance;
+
+@Component
+public class TransferSqlDAO implements TransferDAO {
+
+	private JdbcTemplate jdbcTemplate;
+	
+	public TransferSqlDAO(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public Balance getBalance(int userId) {
+		
+		String sql = "select balance from accounts where user_id = ?";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+		
+		Balance userBalance = new Balance();
+		
+		if (results.next()) {
+			userBalance.setBalance(results.getDouble("balance"));
+			
+		}
+		
+		return userBalance;
+	}
+	
+	
+
+}
